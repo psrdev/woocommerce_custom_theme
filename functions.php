372302a903303849f88dee64eb3726e8137d2bc1
Pravin Singh Rana <dev.psrana@gmail.com>
2026-02-06 11:15:22 +0530

    added theme support for woocmmerce gallery

--- functions.php
+++ functions.php
@@ -482,3 +482,10 @@ add_action('pre_get_posts', function ($query) {
         $query->set('order', 'DESC');
     }
 });
+add_action('after_setup_theme', function () {
+    add_theme_support('woocommerce');
+
+    add_theme_support('wc-product-gallery-slider');
+    add_theme_support('wc-product-gallery-lightbox');
+    add_theme_support('wc-product-gallery-zoom');
+});
